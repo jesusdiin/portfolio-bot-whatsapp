bf2de700f73497060d10b40ff15dd1e043e1882e
Jesús Hernández <cjesus02hdz@gmail.com>
2023-03-29 01:41:54 -0600

    refactor(stickers): :recycle: refactor feature stickers
    refactor code for a scalable code

--- messages/msgResponses.js
+++ messages/msgResponses.js
@@ -1,17 +1,14 @@
-import { decryptMedia } from '@open-wa/wa-automate';
-
-// libs
-import { isUrl, uaOverride } from '../libs/constWhatsApp.lib';
+import cmdSticker from './../commands/sticker.cmd.js';
 
 const msgResponses = async (client, message) => {
 
 	try {
+		// eslint-disable-next-line no-unused-vars
 		const { type, id, from, caption, isMedia, mimetype, quotedMsg } = message;
 		let { body } = message;
 
 		const commands = caption || body || '';
 		const command = commands.toLowerCase().split(' ')[0] || '';
-		const args = commands.split(' ');
 
 		// eslint-disable-next-line no-unused-vars
 		const msgs = (message) => {
@@ -31,23 +28,7 @@ const msgResponses = async (client, message) => {
 		case '!sticker':
 		case '!stk':
 		case '!stkr':
-			if(isMedia && type === 'image') {
-				const mediaData = await decryptMedia(message, uaOverride);
-				const imageBase64 = `data:${mimetype};base64,${mediaData.toString('base64')}`;
-				await client.sendImageAsSticker(from, imageBase64);
-			} else if (quotedMsg && quotedMsg.type == 'image') {
-				const mediaData = await decryptMedia(quotedMsg, uaOverride);
-				const imageBase64 = `data:${quotedMsg.mimetype};base64,${mediaData.toString('base64')}`;
-				await client.sendImageAsSticker(from, imageBase64);			
-			} else if (args.length === 2) {
-				const url = args[1];
-				if (url.match(isUrl)) {
-					await client.sendStickerfromUrl(from, url, {method: 'get'})
-						.catch(err=>console.log('Error: ', err));
-				}
-			} else {
-				client.reply(from, 'Error', id);
-			}
+			await cmdSticker(client, message);
 			break;
 		default:
 			console.log('default');
